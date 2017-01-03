fdf2330bf68e2e4ac088bea60924fa95d7ef7f21
jkganzan13 <jkganzan13@gmail.com>
2017-01-03 22:24:12 +1300

    Feature/material ui (#1)
    replaced bootstrap UI with material UI

--- src/components/sidebar/ReviewsList.js
+++ src/components/sidebar/ReviewsList.js
@@ -1,33 +1,64 @@
 import React from 'react';
 import _ from 'lodash'
-import { Accordion, Badge, Panel } from 'react-bootstrap'
+import { AppBar, Badge, Divider, List, ListItem, Paper, Tab, Tabs} from 'material-ui';
 
-const getPanelHeader = (header, number) => <span>{_.replace(header, '_', ' ')} <Badge>{number}</Badge> </span>;
+const getBadge = (header, number) => (
+  <Badge  badgeContent={number}  secondary={true}>
+    {_.replace(header, '_', ' ')}
+  </Badge>
+);
+
+const styles = {
+  appBar: {
+    flexWrap: 'wrap',
+    zIndex: 0
+  },
+  tabs: {
+    width: '100%',
+  },
+};
 
 const ReviewsList = ({ reviews }) => {
   return (
-    <div>
-      <h4 style={{ color: 'white' }}>Review List:</h4>
-      <Accordion>
+    <Paper style={{height:'480px'}}>
+      <AppBar
+        title="Review List"
+        style={styles.appBar}
+        showMenuIconButton={false}
+        zDepth={0}
+      >
+      </AppBar>
+      <Tabs style={styles.tabs} contentContainerStyle={{overflowY:'scroll', height:'368px'}}>
         {
           _.keys(reviews).map((reviewType, i) => {
             return (
-              <Panel header={getPanelHeader(reviewType, _.size(reviews[reviewType]))} eventKey={i} key={i} >
+              <Tab label={getBadge(reviewType, _.size(reviews[reviewType]))} key={i}>
+                <List>
                 {
                   reviews[reviewType].map((reviewItem, i) => {
                     return (
                       <div key={i}>
-                        <span>{reviewItem.user}: {reviewItem.comment}</span>
+                        <ListItem
+                          primaryText={reviewItem.user}
+                          secondaryText={
+                            <p style={{WebkitLineClamp: 0, height: ''}}>
+                              {reviewItem.comment}
+                            </p>
+                          }
+                          secondaryTextLines={2}
+                        />
+                        <Divider inset={true} />
                       </div>
                     )
                   })
                 }
-              </Panel>
+                </List>
+              </Tab>
             )
           })
         }
-      </Accordion>
-    </div>
+      </Tabs>
+    </Paper>
   )
 };
 

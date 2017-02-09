419c53cc43d53c43d594a84563144b0cc69ead0d
jkganzan13 <jkganzan13@gmail.com>
2017-02-09 22:24:58 +1300

    added note on base.js

--- src/config/base.js
+++ src/config/base.js
@@ -1,3 +1,4 @@
+// Note: Export a firebase.config.js file with your firebase details.
 import firebaseCfg from './firebase.config';
 
 // Settings configured here will be merged into the final config object.
